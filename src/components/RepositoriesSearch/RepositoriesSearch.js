49a8cb8cedc128d41435e41755a2bf899cf135e1
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-10-16 02:02:15 -0400

    update repo search component

--- src/components/RepositoriesSearch/RepositoriesSearch.js
+++ src/components/RepositoriesSearch/RepositoriesSearch.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import { useState } from 'react';
+import { Repo } from '../../services/services';
+import Form from '../Form/Form';
+import RepoCard from '../RepoCard/RepoCard';
+
+import './RepositoriesSearch.scss';
 
 const RepositoriesSearch = () => {
-  return <div>RepositoriesSearch</div>;
+  const [repository, setRepository] = useState('');
+  const [data, setData] = useState([]);
+
+  const onRepoChange = (e) => setRepository(e.target.value);
+
+  const getRepo = () => {
+    Repo.getRepo(repository).then(({ items }) => {
+      setData(items);
+    });
+  };
+
+  const onSearchRepo = (e) => {
+    e.preventDefault();
+    getRepo();
+  };
+
+  return (
+    <div className="search__container">
+      <Form
+        onSubmit={onSearchRepo}
+        value={repository}
+        onChange={onRepoChange}
+      />
+      <div className="repo">
+        {data?.map((repo) => {
+          return (
+            <RepoCard
+              key={repo.id}
+              name={repo.full_name}
+              url={repo.clone_url}
+              user={repo.owner.login}
+              language={repo.language}
+              fork={repo.forks}
+              stars={repo.stargazers_count}
+              description={repo.description}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default RepositoriesSearch;
