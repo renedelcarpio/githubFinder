7d86c18b0f84cfd2cef02139a5d03b221fa48b60
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-10-16 02:02:32 -0400

    update user search component

--- src/components/UserSearch/UserSearch.js
+++ src/components/UserSearch/UserSearch.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
+import { User } from '../../services/services';
+import Form from '../Form/Form';
+import UserCard from '../UserCard/UserCard';
+
+import './UserSearch.scss';
 
 const UserSearch = () => {
-  return <div>UserSearch</div>;
+  const [user, setUser] = useState('');
+  const [data, setData] = useState([]);
+
+  const onUserChange = (e) => setUser(e.target.value);
+
+  const getUsers = () => {
+    User.getUser(user).then(({ items }) => {
+      setData(items);
+    });
+  };
+
+  const onSearchUser = (e) => {
+    e.preventDefault();
+    getUsers();
+  };
+
+  return (
+    <div className="search__container">
+      <Form onSubmit={onSearchUser} value={user} onChange={onUserChange} />
+      <div className="user">
+        {data?.map((person) => {
+          return (
+            <UserCard
+              key={person.id}
+              image={person.avatar_url}
+              name={person.login}
+              url={person.html_url}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default UserSearch;
