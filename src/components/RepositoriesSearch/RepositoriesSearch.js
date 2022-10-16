fed67a723b784e42234ca87cfffbce3f87ea4ca0
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-10-16 18:15:56 -0400

    using error and loading components when get data

--- src/components/RepositoriesSearch/RepositoriesSearch.js
+++ src/components/RepositoriesSearch/RepositoriesSearch.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Repo } from '../../services/services';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import Form from '../Form/Form';
+import Loader from '../Loader/Loader';
 import RepoCard from '../RepoCard/RepoCard';
 
 import './RepositoriesSearch.scss';
@@ -8,17 +10,29 @@ import './RepositoriesSearch.scss';
 const RepositoriesSearch = () => {
   const [repository, setRepository] = useState('');
   const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const onRepoChange = (e) => setRepository(e.target.value);
 
-  const getRepo = () => {
-    Repo.getRepo(repository).then(({ items }) => {
-      setData(items);
-    });
+  const getRepo = async () => {
+    setLoader(true);
+    try {
+      await Repo.getRepo(repository).then(({ items }) => {
+        setData(items);
+        setLoader(false);
+      });
+    } catch (error) {
+      console.log(error);
+      setErrorMessage(`${error.message}, the repository does not exixt.`);
+      setLoader(false);
+    }
   };
 
   const onSearchRepo = (e) => {
     e.preventDefault();
+    setErrorMessage('');
+    setData([]);
     getRepo();
   };
 
@@ -29,22 +43,30 @@ const RepositoriesSearch = () => {
         value={repository}
         onChange={onRepoChange}
       />
-      <div className="repo">
-        {data?.map((repo) => {
-          return (
-            <RepoCard
-              key={repo.id}
-              name={repo.full_name}
-              url={repo.clone_url}
-              user={repo.owner.login}
-              language={repo.language}
-              fork={repo.forks}
-              stars={repo.stargazers_count}
-              description={repo.description}
-            />
-          );
-        })}
-      </div>
+      {loader ? (
+        <Loader />
+      ) : (
+        <div className="repo">
+          {errorMessage ? (
+            <ErrorMessage message={errorMessage} />
+          ) : (
+            data?.map((repo) => {
+              return (
+                <RepoCard
+                  key={repo.id}
+                  name={repo.full_name}
+                  url={repo.clone_url}
+                  user={repo.owner.login}
+                  language={repo.language}
+                  fork={repo.forks}
+                  stars={repo.stargazers_count}
+                  description={repo.description}
+                />
+              );
+            })
+          )}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/UserSearch/UserSearch.js
+++ src/components/UserSearch/UserSearch.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { User } from '../../services/services';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import Form from '../Form/Form';
+import Loader from '../Loader/Loader';
 import UserCard from '../UserCard/UserCard';
 
 import './UserSearch.scss';
@@ -8,35 +10,55 @@ import './UserSearch.scss';
 const UserSearch = () => {
   const [user, setUser] = useState('');
   const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const onUserChange = (e) => setUser(e.target.value);
 
-  const getUsers = () => {
-    User.getUser(user).then(({ items }) => {
-      setData(items);
-    });
+  const getUsers = async () => {
+    setLoader(true);
+    try {
+      await User.getUser(user).then(({ items }) => {
+        setData(items);
+        setLoader(false);
+      });
+    } catch (error) {
+      console.log(error);
+      setErrorMessage(`${error.message}, the user does not exist.`);
+      setLoader(false);
+    }
   };
 
   const onSearchUser = (e) => {
     e.preventDefault();
+    setErrorMessage('');
+    setData([]);
     getUsers();
   };
 
   return (
     <div className="search__container">
       <Form onSubmit={onSearchUser} value={user} onChange={onUserChange} />
-      <div className="user">
-        {data?.map((person) => {
-          return (
-            <UserCard
-              key={person.id}
-              image={person.avatar_url}
-              name={person.login}
-              url={person.html_url}
-            />
-          );
-        })}
-      </div>
+      {loader ? (
+        <Loader />
+      ) : (
+        <div className="user">
+          {errorMessage ? (
+            <ErrorMessage message={errorMessage} />
+          ) : (
+            data?.map((person) => {
+              return (
+                <UserCard
+                  key={person.id}
+                  image={person.avatar_url}
+                  name={person.login}
+                  url={person.html_url}
+                />
+              );
+            })
+          )}
+        </div>
+      )}
     </div>
   );
 };
